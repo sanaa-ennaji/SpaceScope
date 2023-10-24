c3a706a85b0a17ac74312ca49f823a4e7dfae4a3
sanaa ennaji <sanaaennaji93@gmail.com>
2023-10-24 15:48:19 +0100

    MAKE IT RESPONSIVE

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ btns.forEach(btn=>{
         btn.parentNode.classList.toggle('active');
     })
 })
+const burgerMenu = document.querySelector('.burger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+burgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
