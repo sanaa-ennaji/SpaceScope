0334de5f44badb68dc8b5d628b2657522a49a293
sanaa ennaji <sanaaennaji93@gmail.com>
2023-10-25 17:57:53 +0100

    VALIDATION DES INPUTES

--- script.js
+++ script.js
@@ -1,60 +1,69 @@
-const btns = document.querySelectorAll('.faq-toggle');
+  //FAQ
+  const btns = document.querySelectorAll('.faq-toggle');
 
 btns.forEach(btn=>{
     btn.addEventListener('click',()=>{
         btn.parentNode.classList.toggle('active');
     })
 })
+//menu
 const burgerMenu = document.querySelector('.burger-menu');
 const navLinks = document.querySelector('.nav-links');
 
 burgerMenu.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
-// // Get form elements
-// const form = document.getElementById('myForm');
-// const nameInput = document.getElementById('name');
-// const emailInput = document.getElementById('email');
-// const phoneInput = document.getElementById('phone');
-// const messageInput = document.getElementById('message');
+//Validation
+function submitForm() {
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var message = document.getElementById("message").value;
+    var outputDiv = document.getElementById('output');
+    var reg = /^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/;
 
-// function validateEmail(email) {
-//     const emailRegex = /^\S+@\S\.\S/;
-//     return emailRegex.test(email);
-// }
-// function validatePhone(phone) {
-//     return phone.trim() !== '';
-// }
+    document.getElementById("emailError").innerHTML = '';
+    document.getElementById("phoneError").innerHTML = '';
+    document.getElementById("messageError").innerHTML = '';
 
-// form.addEventListener('submit', function (event) {
-//     event.preventDefault();
+    var isValid = true;
 
-//     let valid = true;
+    if (email == "" || !reg.test(email)) {
+        document.getElementById("emailError").innerHTML = " invalid email you miss @ and .";
+        isValid = false;
+    }
 
-//     if (nameInput.value.trim() === '') {
-//         alert('Please enter your full name.');
-//         valid = false;
-//     }
+    if (phone == "") {
+        document.getElementById("phoneError").innerHTML = "Phone number is empty.";
+        isValid = false;
+    }
 
-//     if (!validateEmail(emailInput.value)) {
-//         alert('Please enter a valid email address.');
-//         valid = false;
-//     }
+    else if (!/^(\+212[-]?\d{9,})$/.test(phone)) {
+        document.getElementById("phoneError").innerHTML = " start with '+212' and contain 9 numbers and without space ";
+        isValid = false;
+    }
 
-//     if (!validatePhone(phoneInput.value)) {
-//         alert('Please enter a valid phone number.');
-//         valid = false;
-//     }
+    if (message == "") {
+        document.getElementById("messageError").innerHTML = "empty";
+        isValid = false;
+    }
 
-//     if (messageInput.value.trim() === '') {
-//         alert('Please enter your message.');
-//         valid = false;
-//     }
+    if (isValid) {
+        var outputText = "Email: " + email + "<br>Phone: " + phone + "<br>Text: " + message + "<br><br>";
+        outputDiv.innerHTML += outputText;
 
-//     if (valid) {
-     
-//         alert('Form submitted successfully.');
-//         form.reset(); 
-//     }
-// });
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("message").value = "";
+    }
+    else {
+        outputDiv.innerHTML = '';
+    }
+}
+
+const form = document.getElementById('myForm');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    submitForm();
+});
+
 
